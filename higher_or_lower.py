308c953285d25f497e5bcc92ddceb69cc6a2afc7
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2023-02-23 15:09:49 -0500

    exercise 2 completed

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -1,1 +1,11 @@
+from random import randrange
+
 # Exercise 2
+number = randrange(10)
+guess = int(input('Guess a number between 0 and 10: '))
+if guess < number:
+    print('Your guess was too low. The number was: ' + str(number))
+elif guess > number:
+    print('Your guess was too high. The number was: ' + str(number))
+else:
+    print('Your guess is correct! The number is: ' + str(number))
