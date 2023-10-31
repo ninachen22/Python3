500d69524fd08026fe5e9a88a24cc5dda4379df7
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2023-10-31 14:56:02 -0400

    cleared up code

--- multilingual_greeter.py
+++ multilingual_greeter.py
@@ -7,13 +7,6 @@ def language_input():
     language = input("Please choose a langauge: English, Spanish, Chinese\n")
     return language
 
-def name_input():
-    """
-    asks user to input name in chosen language and returns input as string
-    """
-    name = input("Please enter your name in the chosen language: ")
-    return name
-
 def greet(language):
     if language == "English":
         name = input("Please enter your name: ")
